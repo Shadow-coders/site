e7316687de6ca37acb3e8dc5309d8fe9926f7415
neon <saahilattud@gmail.com>
2021-10-05 11:58:49 -0400

    Update api.ts

--- api/routes/api.ts
+++ api/routes/api.ts
@@ -2,10 +2,10 @@ import { Router } from 'express';
 import BasicRouter from './basic'
 import { Canvas } from 'canvas-constructor/cairo'
 import canvas from 'canvas'
-import app from '..';
+
 const router = Router();
 router.use('/basic', BasicRouter);
-router.get('/', (req:any,res:any) => res.json({ version: 1.00 }))
+router.get('/', (req:any,res:any) => res.json({ version: 1.00, message: '' }))
 router.get('/testimage', async (req,res) => {
     const img = await canvas.loadImage('https://th.bing.com/th/id/OIP.v8mzpYYKhQjNP0NYqp2DDwHaFq?pid=ImgDet&rs=1')
     let image = new Canvas(474,362)
