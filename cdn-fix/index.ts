90fb7c937b227aec2781ed81a8d12aa8b4ffffd8
neon <saahilattud@gmail.com>
2021-10-05 18:31:01 -0400

    e

---
+++ cdn-fix/index.ts
@@ -0,0 +1,38 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = express();
+const baseurl = 'UR URL'
+const PORT = 8000;
+export default app;
+app.use('/files/', express.static('uploads'))
+// default options
+app.use(fileUpload());
+app.get('/ping', function(req:any, res:any) {
+  res.send('pong');
+});
+
+
+app.post('/upload', function(req:any, res:any) {
+//@ts-ignore
+    let sampleFile;
+  let uploadPath;
+console.log(req.files)
+  if (!req.files || Object.keys(req.files).length === 0) {
+    res.status(400).send('No files were uploaded.');
+    return;
+  }
+ // eslint-disable-line
+// console.log(req.body, req.files) debug
+  sampleFile = req.files.sampleFile;
+
+  uploadPath = __dirname + '/uploads/' + sampleFile.name;
+
+  sampleFile.mv(uploadPath, function(err:any) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.send('Done')
+    });
+
+});
+app.on('mount', () => console.log('CDN MOUNTED'))
