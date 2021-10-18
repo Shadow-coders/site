244ff97d9409be6235ce77811f1fe12a477cd052
Neon <58448036+NeonGamerBot-QK@users.noreply.github.com>
2021-10-18 17:17:03 -0400

    e

--- routes/dashboard-guild.ts
+++ routes/dashboard-guild.ts
@@ -3,6 +3,7 @@ import config from "../config"
 export default {
     name: '/dashboard/:id',
     async execute(req:any,res:any,client:any) {
+        if(!req.user) res.status(403).json({ message: 403, code: 4.03 })
         const guild:Guild = client.shadow.guilds.cache.get(req.params.id)
         const header:any = await client.add('partials/header-dashboard.ejs', { config, client, req,res, user: req.user, avatar: `https://cdn.discordapp.com/avatars/${req.user?.id}/${req.user?.avatar}.gif?width=214&height=214`})
 res.render('settings', { req,res,client: client.shadow, db: client.db, guild, header, config })
--- routes/settings.ts
+++ routes/settings.ts
@@ -2,8 +2,9 @@ import config from '../config'
 export default {
     name: '/settings',
     execute: (req:any,res:any,bot:any) => {
-	console.log('RES on user-settings');
-		const user = req.user
+//	console.log('RES on user-settings');
+const header:any = await client.add('partials/header-dashboard.ejs', { config, client, req,res, user: req.user, avatar: `https://cdn.discordapp.com/avatars/${req.user?.id}/${req.user?.avatar}.gif?width=214&height=214`})
+const user = req.user
         res.render('user-settings', {
 			avatar: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.gif?width=214&height=214`,
 			username: user.username,
