3a6d886500704305a6b8c2a14e1cbb3b2511a339
Neon <58448036+NeonGamerBot-QK@users.noreply.github.com>
2021-10-18 17:18:33 -0400

    d

--- routes/settings.ts
+++ routes/settings.ts
@@ -3,7 +3,7 @@ export default {
     name: '/settings',
     execute: (req:any,res:any,bot:any) => {
 //	console.log('RES on user-settings');
-const header:any = await client.add('partials/header-dashboard.ejs', { config, client, req,res, user: req.user, avatar: `https://cdn.discordapp.com/avatars/${req.user?.id}/${req.user?.avatar}.gif?width=214&height=214`})
+const header:any = await client.add('partials/header-dashboard.ejs', { config, client: bot.shadow, req,res, user: req.user, avatar: `https://cdn.discordapp.com/avatars/${req.user?.id}/${req.user?.avatar}.gif?width=214&height=214`})
 const user = req.user
         res.render('user-settings', {
 			avatar: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.gif?width=214&height=214`,
