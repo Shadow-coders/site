99d871ce9e07e399998aba9102ba18c398c9b14d
Neon <58448036+NeonGamerBot-QK@users.noreply.github.com>
2021-10-19 16:42:18 -0400

    Update modules.ts

--- handlers/modules.ts
+++ handlers/modules.ts
@@ -6,13 +6,13 @@ app.use(Express.static('public'))
 app.engine('html', require('ejs-locals'));
 app.use(Express.json())
 app.use(session({
-    secret: `fojwbowubfouwbfouwbfowuebfowuebfwouefbowfubweoufbweoufbwoefbuwoefub`,
+    secret: ['e', 'e', 'e', 'nsiefnsifwesgwagewsgsgesege/sg/esr/g/esrg/e/srg/eth/tt/a/#W$?#Y?a?/3Y?Yr/Y?W$Y/4y/5e/Y/4?', 1.0, 5].join(),
     resave: false,
     saveUninitialized: false,
     cookie: {
         httpOnly: true,
         secure: false,
-        maxAge: 48 * 60 * 60 * 1000,
+        maxAge: 48 * 60 * 60 * 1000, // 48 hours | 2 days
     },
 }))
 app.use(requestHandler)
