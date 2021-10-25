5009ea72761697914784d5e4a02575a5d7ae1b9e
neon <saahilattud@gmail.com>
2021-10-25 15:16:37 -0400

    eee

--- api/routes/Meme.ts
+++ api/routes/Meme.ts
@@ -6,13 +6,16 @@ const router = express.Router();
 router.all('/', (req:any,res:any) => {
 res.json({ version: 0x0, /* VERSION NUMBER */ message: '', endpoints: ['GET /test-endpoint', 'POST /endpoint']  })
 })
-router.get('/test', async (req, res) => {
+router.get('/oogway', async (req, res) => {
+if(!req.query.text) return res.status(400).json({ 
+    status: 400,
+    message: 'Invalid query ?text='
+})
     const img = await canvas.loadImage('https://shadow-bot.dev/cdn/files/d4c6a5723891d4fcddca133e0cb1383b.png')
     let image = new Canvas(474,362)
     .printImage(img, 0, 0, 474, 362)
     .setTextFont('15px Impact')
-    .printText('Hello', 211, 50)
-    .printText('Sus', 211, 227)
+    .printText((req.query.text as string), 210, 220)
     .toBuffer();
 
     res.set({'Content-Type': 'image/png'})
